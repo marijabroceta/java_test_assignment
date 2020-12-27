c7f9de8c382ad62aeb0bfafc26840ee8a34c708a
Marija Broceta <broceta.marija@gmail.com>
2020-12-27 18:22:40 +0100

    Created PageObject and FunctionalTest classes

---
+++ src/test/java/FunctionalTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class FunctionalTest {
+
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void cleanUp(){
+        driver.manage().deleteAllCookies();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
+}
